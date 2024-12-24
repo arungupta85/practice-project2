a76363be51b41356777c77d8df9d51de4aa0087c
Arun <arun.sdtc@gmail.com>
2024-12-24 22:18:36 +0530

    msg

---
+++ src/main/java/com/demo2/practice_project2/PracticeProject2Application.java
@@ -0,0 +1,13 @@
+package com.demo2.practice_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo2/practice_project2/PracticeProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo2.practice_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
